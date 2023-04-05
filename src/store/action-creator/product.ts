6277ab7d5a6676b6162d080a6a7d13d78d2e1316
Ilham Zeynalov <ilhamzeynalov65@gmail.com>
2023-04-05 16:48:47 +0400

    add localstorage

--- src/store/action-creator/product.ts
+++ src/store/action-creator/product.ts
@@ -9,8 +9,8 @@ export const fetchProducts = () => {
             const response = await axios.get('./data.json')
             let products = JSON.parse(localStorage.getItem('products') as any)
             if (products === null) products = []
-            // if (products.length === 0)
-            //     localStorage.setItem('products', JSON.stringify(response.data))
+            if (products.length === 0)
+                localStorage.setItem('products', JSON.stringify(response.data))
             dispatch({ type: ProductActionTypes.FETCH_PRODUCTS_SUCCESS, payload: products.length === 0 ? response.data : products })
         } catch (e) {
             dispatch({ type: ProductActionTypes.FETCH_PRODUCTS_ERROR, payload: 'Error' })
