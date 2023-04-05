e7a004c214c6abcab60c08dab7e07dd127b3cfac
Ilham Zeynalov <ilhamzeynalov65@gmail.com>
2023-04-05 16:41:40 +0400

    change data url

--- src/store/action-creator/product.ts
+++ src/store/action-creator/product.ts
@@ -6,11 +6,11 @@ export const fetchProducts = () => {
     return async (dispatch: Dispatch<IProductAction>) => {
         try {
             dispatch({ type: ProductActionTypes.FETCH_PRODUCTS })
-            const response = await axios.get('/data.json')
+            const response = await axios.get('./data.json')
             let products = JSON.parse(localStorage.getItem('products') as any)
             if (products === null) products = []
-            if (products.length === 0)
-                localStorage.setItem('products', JSON.stringify(response.data))
+            // if (products.length === 0)
+            //     localStorage.setItem('products', JSON.stringify(response.data))
             dispatch({ type: ProductActionTypes.FETCH_PRODUCTS_SUCCESS, payload: products.length === 0 ? response.data : products })
         } catch (e) {
             dispatch({ type: ProductActionTypes.FETCH_PRODUCTS_ERROR, payload: 'Error' })
